6848ae345cf266f0b0f388293fe4dc2b5dfd7b13
awarestudio <awarestudio.cc@gmail.com>
2023-04-06 08:55:30 +0200

    Added setting and starting grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  // put setup code here
+	noLoop()
 }
 
 function draw() {
-  // put drawing code here
+	createCanvas(CANVAS.WIDTH, CANVAS.HEIGHT)
+	background(BACKGROUND_COLOR)
+
+	topLeft = createVector(CANVAS.MARGINS.LEFT, CANVAS.MARGINS.TOP)
+	bottomRight = createVector(CANVAS.MARGINS.RIGHT, CANVAS.MARGINS.BOTTOM)
+
+	grid = Grid2D.fromTwoVertices(topLeft, bottomRight, 10, 10)
+	grid.debug()
+	print(grid.xStep)
+	print(grid.yStep)
 }
