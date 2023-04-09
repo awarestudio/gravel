81e33f5187b85595e30e347ae098b17328eeb626
awarestudio <awarestudio.cc@gmail.com>
2023-04-09 18:46:44 +0200

    Drawn grid of rectangles

--- sketch.js
+++ sketch.js
@@ -1,16 +1,29 @@
 function setup() {
 	noLoop()
+
+	rectanglesLayer = createGraphics(CANVAS.WIDTH, CANVAS.HEIGHT)
+	rectanglesLayer.rectMode(CENTER)
 }
 
 function draw() {
 	createCanvas(CANVAS.WIDTH, CANVAS.HEIGHT)
 	background(BACKGROUND_COLOR)
 
-	topLeft = createVector(CANVAS.MARGINS.LEFT, CANVAS.MARGINS.TOP)
-	bottomRight = createVector(CANVAS.MARGINS.RIGHT, CANVAS.MARGINS.BOTTOM)
+	topLeft = createVector(CANVAS.MARGINS_POSITION.LEFT+SQUARE_SIZE/2, CANVAS.MARGINS_POSITION.TOP+SQUARE_SIZE/2)
+	positionsGrid = Grid2D.fromOnePoint(topLeft, N_SQUARES_V, N_SQUARES_H, SQUARE_SIZE, SQUARE_SIZE)
 
-	grid = Grid2D.fromTwoVertices(topLeft, bottomRight, 10, 10)
-	grid.debug()
-	print(grid.xStep)
-	print(grid.yStep)
+	for (let vIndex = 0; vIndex < positionsGrid.points.length; vIndex++) {
+		const hpoints = positionsGrid.points[vIndex]
+		for (let hIndex = 0; hIndex < hpoints.length; hIndex++) {
+			const position = hpoints[hIndex]
+			rectanglesLayer.push()
+				rectanglesLayer.translate(position)
+				rectanglesLayer.rect(0, 0, SQUARE_SIZE, SQUARE_SIZE)
+			rectanglesLayer.pop()
+		}
+	}
+	
+	image(rectanglesLayer, 0, 0)
+	
+	// positionsGrid.debug()
 }
